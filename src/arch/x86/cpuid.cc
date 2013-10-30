b696a6dffd81dd477834232392060fa2fd6734f8
Tony Nowatzki <tjn@cs.wisc.edu>
2013-10-30 11:36:06 -0500

    icc option

--- src/arch/x86/cpuid.cc
+++ src/arch/x86/cpuid.cc
@@ -63,6 +63,7 @@ namespace X86ISA {
     };
 
     static const int vendorStringSize = 13;
+    // It is not GenuineIntel...
     static const char vendorString[vendorStringSize] = "GenuineIntel";
     static const int nameStringSize = 48;
     static const char nameString[nameStringSize] = "Fake M5 x86_64 CPU";
@@ -150,10 +151,13 @@ namespace X86ISA {
                         stringToRegister(vendorString + 8));
                 break;
               case FamilyModelStepping:
-                //result = CpuidResult(0x00020f51, 0x00000805,
-                //                     0xe7dbfbff, 0x00000001);
-                result = CpuidResult(0x000306a9, 0x05100800,
+                if (getenv("GEM5_X86_CPUID_HACK") == 0) {
+	           result = CpuidResult(0x00020f51, 0x00000805,
+                                     0xe7dbfbff, 0x00000001);
+                } else {
+	           result = CpuidResult(0x000306a9, 0x05100800,
                                      0x7f9ae3bf,0xbfebfbff);
+                }
                 break;
               default:
                 warn("x86 cpuid: unimplemented function %u", funcNum);
