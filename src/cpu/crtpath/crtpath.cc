354ee5f9ccb584ef15d10fd2cb7d251de31299c4
Tony Nowatzki <tjn@cs.wisc.edu>
2014-04-17 23:20:06 -0500

    increased srcreg size to 9

--- src/cpu/crtpath/crtpath.cc
+++ src/cpu/crtpath/crtpath.cc
@@ -239,7 +239,7 @@ void CP_Graph::consumer(unsigned reg, uint64_t seq, unsigned i)
     return;
 
   assert(i>=0);
-  
+
   assert(i<MAX_SRC_REGS);
   uint64_t regseq = reg_to_producer[reg];
   if (regseq) {
